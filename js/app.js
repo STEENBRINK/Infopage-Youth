4f68af91ebd4dc8adfd5373c5f732ee121196488
STEENBRINK <bram.steenbrink@hotmail.com>
2019-06-14 17:21:43 +0200

    Cleaned up a lot of code, getting ready to deploy v1

--- js/app.js
+++ js/app.js
@@ -3,24 +3,28 @@
 //global vars
 var isDrawn = {
     leeftijden: false,
-    bbdg: false
+    bbdg: false,
+    omp: false,
+    vmg: false,
+    zdb: false,
+    kdb: false,
+    gids: false,
+    ioeg: false
 }
 
-var charts = []
+var drawn_charts = []
+var button_register_number = 0
+var buttons = []
 
-//button elements
-var leeftijden_button = document.getElementById("leeftijden_button");
-var bbdg_button = document.getElementById("bbdg_button")
-
-//event listeners
-leeftijden_button.addEventListener("click", (e) => checkChart(e, 'leeftijden_chart', configs.leeftijden, "leeftijden"))
-bbdg_button.addEventListener("click", (e) => checkChart(e, 'bbdg_chart', configs.bbdg, "bbdg"))
+//register buttons and eventlisteners
+registerBarCharts()
+registerPieCharts()
 
 function drawChart(chartElement, config, chartName){
-    charts[chartName] = new Chart(chartElement, config);
+    drawn_charts[chartName] = new Chart(chartElement, config);
 }
 
-function deleteChart(chartElement){
+function deleteChart(chartElement, isDoughnut){
 
     let div = chartElement.parentNode
     let element
@@ -34,16 +38,19 @@ function deleteChart(chartElement){
 
     let newElement = document.createElement('canvas')
     newElement.setAttribute("id", chartElement.id)
+    if(isDoughnut){
+        newElement.setAttribute("class", "doughnut")
+    }
     
     div.removeChild(chartElement);
     div.removeChild(element)
     div.appendChild(newElement);
 }
 
-function checkChart(e, chart, config, drawn){
+function checkChart(e, chart, config, drawn, isDoughnut){
     let chartElement = document.getElementById(chart)
     if(isDrawn[drawn]){
-        deleteChart(chartElement)
+        deleteChart(chartElement, isDoughnut)
         isDrawn[drawn] = false
     }else{
         chartElement.getContext('2d')
@@ -51,3 +58,67 @@ function checkChart(e, chart, config, drawn){
         isDrawn[drawn] = true
     }
 }
+
+function registerBarCharts() {
+    for (let chart_data in data.barCharts){
+    
+        buttons[button_register_number] = document.getElementById(data.barCharts[chart_data].name + "_button")
+        
+        let config
+    
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+            if(data.barCharts[chart_data].five) {
+                config = {
+                    data: data.barCharts[chart_data].data,
+                    type: options.horizontalBar,
+                    options: options.five.options
+                }
+            }else{
+                config = {
+                    data: data.barCharts[chart_data].data,
+                    type: options.horizontalBar,
+                    options: options.ten.options
+                }
+            }
+
+            config.options.title.text = data.barCharts[chart_data].title
+
+            buttons[button_register_number].addEventListener("click", (e) => checkChart(e, data.barCharts[chart_data].name + "_chart", config, data.barCharts[chart_data].name, false))
+        }else{  
+            if(data.barCharts[chart_data].five) {
+                config = {
+                    data: data.barCharts[chart_data].data,
+                    type: options.bar,
+                    options: options.five.options
+                }
+            }else{
+                config = {
+                    data: data.barCharts[chart_data].data,
+                    type: options.bar,
+                    options: options.ten.options
+                }
+            }
+
+            config.options.title.text = data.barCharts[chart_data].title
+            console.log(data.barCharts[chart_data].title)
+
+            buttons[button_register_number].addEventListener("click", (e) => checkChart(e, data.barCharts[chart_data].name + "_chart", config, data.barCharts[chart_data].name, false))
+        }
+
+        button_register_number++
+
+    }
+}
+
+function registerPieCharts() {
+    for (let chart_data in data.pieCharts) {
+
+        buttons[button_register_number] = document.getElementById(data.pieCharts[chart_data].name + "_button")
+
+        buttons[button_register_number].addEventListener("click", (e) => checkChart(e, data.pieCharts[chart_data].name + "_chart", data.pieCharts[chart_data].config, data.pieCharts[chart_data].name, true))
+
+    }
+}
+
+
+
